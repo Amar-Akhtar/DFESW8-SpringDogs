8108b6cde2fb6e2526ca60d732580fac87586f73
lifebeginsat30 <amar.akhtar@icloud.com>
2022-02-07 13:08:10 +0000

    initial commit

---
+++ src/test/java/com/qa/dogs/Dfesw8SpringDogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.dogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dfesw8SpringDogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
